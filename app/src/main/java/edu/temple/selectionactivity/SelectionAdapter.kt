a0ad87663f68d7c9dfa01b2ccf9fd2f77b2f0709
stephull <shullender0907@gmail.com>
2021-10-05 15:49:18 -0400

    added strings, change adapter code (interface, make new onclicklistener)

--- app/src/main/java/edu/temple/selectionactivity/SelectionAdapter.kt
+++ app/src/main/java/edu/temple/selectionactivity/SelectionAdapter.kt
@@ -1,35 +1,41 @@
 package edu.temple.selectionactivity
 
-import android.content.Context
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.recyclerview.widget.RecyclerView
+import android.content.*
+import android.view.*
+import android.widget.*
+import androidx.recyclerview.widget.*
 
-class SelectionAdapter (_context : Context, _imageList : ArrayList<MyPicture>, _myOnClick: View.OnClickListener) : RecyclerView.Adapter<SelectionAdapter.MyViewHolder>() {
-    // declare variables from parameters above
+class SelectionAdapter (_context : Context, _imageList : ArrayList<MyPicture>) : RecyclerView.Adapter<SelectionAdapter.MyViewHolder>() {
+    // declare variables
     private val context = _context
     private val imageList = _imageList
-    private val myOnClick = _myOnClick
 
-    // declare width + height of layout params
-    private val wh = 300
+    interface OnItemClickListener {
+        fun onItemClick(pos : Int)
+    }
+
+    private lateinit var myListener : OnItemClickListener
+    fun setOnItemClickListener(listener : OnItemClickListener) {
+        myListener = listener
+    }
 
     // view holder class
-    class MyViewHolder(_view: ImageView, myOnClick: View.OnClickListener) : RecyclerView.ViewHolder(_view) {
-        val imageView = _view.apply {setOnClickListener(myOnClick)}
+    class MyViewHolder(view: View, listener: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+        val recycleViewImage: ImageView = view.findViewById(R.id.recyclerViewImage)
+        init {
+            view.setOnClickListener { listener.onItemClick(adapterPosition) }
+        }
     }
 
     // create view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        return MyViewHolder(ImageView(context).apply {
-            layoutParams = ViewGroup.LayoutParams(wh, wh)
-        }, myOnClick)
+        val holderItem = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview, parent, false)
+        return MyViewHolder(holderItem, myListener)
     }
 
     // bind view holder
     override fun onBindViewHolder(holder: MyViewHolder, pos: Int) {
-        holder.imageView.setImageResource(imageList[pos].imageSrc)
+        holder.recycleViewImage.setImageResource(imageList[pos].imageSrc)
     }
 
     // return the size of image list
